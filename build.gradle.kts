106a43c4398988abcbb5985403042ce2cb4c53ed
jesuscb123 <jconbar633@g.educaand.es>
2025-04-11 00:19:40 +0200

    empaquetado y plugin metido.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,11 @@
 plugins {
     kotlin("jvm") version "2.0.10"
+    application
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+application {
+    mainClass.set("es.prog2425.dam1.calculadora.MainKt")
 }
 
 group = "prog2425.dam1.prueba-calc-propio"
