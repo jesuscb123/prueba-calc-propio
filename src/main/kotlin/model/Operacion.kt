140b0d67fbfbef2bad84a378e0e1056d626b0eff
jesuscb123 <cbjesus@iesrafaelalberti.es>
2025-04-30 18:04:16 +0200

    programa funcionando con ficheros

--- src/main/kotlin/model/Operacion.kt
+++ src/main/kotlin/model/Operacion.kt
@@ -3,6 +3,6 @@ package prog2425.dam1.calculadora.model
 data class Operacion(val numero: Double, val signo: String, val numero2: Double, val resultado: Double) {
 
     override fun toString(): String {
-        return "Primer número: $numero, Operador: $signo, Segundo número: $numero2, Resultado: $resultado"
+        return "Primer número: $numero, Operador: $signo, Segundo número: $numero2, Resultado: $resultado\n"
     }
 }
